3b775d03193ab932b078a778912de088979c287a
sumit <mazumdarsumit3@gmail.com>
2026-03-17 18:49:46 +0530

    build: Downgrade Solidity pragma from 0.8.34 to 0.8.28 in deployment scripts.

--- script/tSwap/DeployTSwap.s.sol
+++ script/tSwap/DeployTSwap.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.34;
+pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {PoolFactory} from "../../src/audits/tSwap/PoolFactory.sol";
